9c93fa7610a3c6713884c60ad544aa1950f55e00
Caesar0815 <gingermk007@gmail.com>
2020-11-19 10:30:46 +0100

    initial commit

---
+++ AppHocBeaconMap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div ref="outerContainer">
+    <div id="container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AppHocBeaconMap",
+  mounted() {
+    let width = this.$refs.outerContainer.offsetWidth;
+    let height = this.$refs.outerContainer.offsetHeight;
+    let stage = new Konva.Stage({
+      container: 'container',
+      width: width,
+      height: height,
+      draggable: true
+    });
+  }
+}
+</script>
